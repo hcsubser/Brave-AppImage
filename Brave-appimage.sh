dada0f60d9b811a92cecd4edb01bff7fbe422cca
mmmdeb <mmilan@linuxmail.org>
2026-07-07 06:32:13 +0200

    test promena

--- Brave-appimage.sh
+++ Brave-appimage.sh
@@ -13,3 +13,6 @@ cd ..
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
 chmod +x appimagetool-x86_64.AppImage
 ./appimagetool-x86_64.AppImage --comp gzip brave-browser.appdir -n -u 'gh-releases-zsync|hcsubser|Brave-AppImage|continuous|Brave*.AppImage.zsync'
+
+
+#twest blablabla
